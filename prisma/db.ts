4477ade68a9abb5818e61e8743cae4a786a3a40f
CodeVansh-22 <vanshchauhand@gmail.com>
2025-08-11 13:02:41 +0000

    Learning

--- prisma/db.ts
+++
@@ -1,8 +0,0 @@
-// prisma/db.ts
-// This file exports a singleton PrismaClient instance for use in your backend
-
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export default prisma;
